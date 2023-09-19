24350799c292dab7e8ef4cddd5d65a7d96243a86
Braejan Arias <ing.brayanarias@gmail.com>
2023-09-19 13:19:36 -0500

    :construction: Login page first approach.

--- frontend/certech-web/src/components/Footer.tsx
+++ frontend/certech-web/src/components/Footer.tsx
@@ -1,6 +1,21 @@
 function Footer() {
   return (
-    <p className="text-xs">Footer</p>
+    <footer
+      className="w-full bg-gray-100 px-6 py-3 font-light"
+    >
+      <div
+        className="flex"
+      >
+        <span
+          className="w-full p-[0.25px] bg-gray-200 "
+        />
+      </div>
+      <p
+      className="text-gray-400 text-sm items-center flex justify-center pt-4"
+      >
+        Liwasi EdTech es un proyecto educativo rural hecho en Colombia.
+      </p>
+    </footer>
   )
 }
 
